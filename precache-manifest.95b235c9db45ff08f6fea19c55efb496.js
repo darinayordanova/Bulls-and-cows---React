1166e752b72b65f4ee601456d8a3cd27fe3f7593
Darina Yordanova <darinayordanova-itsoft>
2021-01-08 11:39:57 +0200

    Updates

---
+++ precache-manifest.95b235c9db45ff08f6fea19c55efb496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77053da542ae66627675f6a18e92fb9d",
+    "url": "/Bulls-and-cows---React/index.html"
+  },
+  {
+    "revision": "4428ab6dd1da21f3c4da",
+    "url": "/Bulls-and-cows---React/static/css/main.2e175239.chunk.css"
+  },
+  {
+    "revision": "c6580005c8a6c3d49cc0",
+    "url": "/Bulls-and-cows---React/static/js/2.e633524d.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/Bulls-and-cows---React/static/js/2.e633524d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4428ab6dd1da21f3c4da",
+    "url": "/Bulls-and-cows---React/static/js/main.3458f9a6.chunk.js"
+  },
+  {
+    "revision": "90da99123beaa3f9fd36",
+    "url": "/Bulls-and-cows---React/static/js/runtime-main.8246fa16.js"
+  }
+]);
